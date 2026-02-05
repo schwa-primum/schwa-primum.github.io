b041aa84ea6603acfd38e50f92f000e59413798e
schwa-primum <djordje.boz@gmail.com>
2026-02-05 12:09:35 +0000

    Deploying to gh-pages from @ schwa-primum/schwa-primum.github.io@5648f36c185c14a4c07d53d2a1e5e85d8f88e9ef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-bio",
           title: "bio",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/bio/";
