f13185772bf25c2b74ef1b04429a22256a4ef80b
schwa-primum <djordje.boz@gmail.com>
2026-02-05 12:00:16 +0000

    Deploying to gh-pages from @ schwa-primum/schwa-primum.github.io@4feff3ae254e9bc6c9e7fa64c2a3b20e96ea998f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-bio",
-    title: "bio",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -12,13 +12,6 @@ ninja.data = [{
   },{id: "nav-about",
           title: "about",
           description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/";
-          },
-        },{id: "nav-bio",
-          title: "bio",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/";
